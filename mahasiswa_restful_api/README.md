79a4d6f17826f888cddf18869b2f611eb52073c1
ekoaulian <ekoas1997@gmail.com>
2019-05-25 06:20:58 +0700

    RESTful-API

---
+++ mahasiswa_restful_api/README.md
@@ -0,0 +1,36 @@
+Cara running
+- buka menggunakan git bash
+- ketik node index
+- jalankan
++ Method GET(untuk melihat semua data)
+http://localhost:3000/api/mahasiswa_api_nilais 
+
++ Method GET(untuk melihat data dengan id 1)
+http://localhost:3000/api/mahasiswa_api_nilais/1 
+
++ Method POST(untuk menambah data menggunakan postman)
+http://localhost:3000/api/mahasiswa_api_nilais 
+body -> raw -> pilih text menjadi JSON (application/json)
+masukkan:
+{
+	"mahasiswa_name": "eko ajah",
+	"mahasiswa_nilai": 76
+}
+lalu SEND
+
++ Method PUT(untuk edit data dengan id 6 saya edit nama)
+http://localhost:3000/api/mahasiswa_api_nilais/6 
+body -> raw -> pilih text menjadi JSON (application/json)
+masukkan:
+{
+	"mahasiswa_name": "eko sapp",
+	"mahasiswa_nilai": 76
+}
+lalu SEND
+
++ Method DELETE(untuk melihat data dengan id 6)
+http://localhost:3000/api/mahasiswa_api_nilais/6
+
+Selamat mencoba wks~ 
+
+
---
+++ mahasiswa_restful_api/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
+ 
+// parse application/json
+app.use(bodyParser.json());
+ 
+//create database connection
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mhs_crud'
+});
+
+//connect to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
+
+//tampilkan semua data mahasiswa
+app.get('/api/mahasiswa_api_nilais',(req, res) => {
+  let sql = "SELECT * FROM mahasiswa_api_nilai";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+ 
+//tampilkan data mahasiswa berdasarkan id
+app.get('/api/mahasiswa_api_nilais/:id',(req, res) => {
+  let sql = "SELECT * FROM mahasiswa_api_nilai WHERE mahasiswa_id="+req.params.id;
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+
+//Tambahkan data mahasiswa baru
+app.post('/api/mahasiswa_api_nilais',(req, res) => {
+  let data = {mahasiswa_name: req.body.mahasiswa_name, mahasiswa_nilai: req.body.mahasiswa_nilai};
+  let sql = "INSERT INTO mahasiswa_api_nilai SET ?";
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+
+//Edit data mahasiswa berdasarkan id
+app.put('/api/mahasiswa_api_nilais/:id',(req, res) => {
+  let sql = "UPDATE mahasiswa_api_nilai SET mahasiswa_name='"+req.body.mahasiswa_name+"', mahasiswa_nilai='"+req.body.mahasiswa_nilai+"' WHERE mahasiswa_id="+req.params.id;
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+ 
+//Delete data mahasiswa berdasarkan id
+app.delete('/api/mahasiswa_api_nilais/:id',(req, res) => {
+  let sql = "DELETE FROM mahasiswa_api_nilai WHERE mahasiswa_id="+req.params.id+"";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+ 
+//Server listening
+app.listen(3000,() =>{
+  console.log('Server started on port 3000...');
+});
