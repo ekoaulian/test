7ee82a35503d64100559f89a0a9a619c4453258b
ekoaulian <ekoas1997@gmail.com>
2019-11-26 23:45:15 +0700

    cp3

--- cp/kondisi/3.php
+++ cp/kondisi/3.php
@@ -1,1 +1,16 @@
-3.php
+<?php 
+
+	$nilai = 90;
+
+	if ($nilai >= 80) {
+		echo "Nilai Anda A";
+	} elseif ($nilai >= 70) {
+		echo "Nilai Anda B";
+	} elseif ($nilai >= 50) {
+		echo "Nilai Anda C";
+	} elseif ($nilai >= 40) {
+		echo "Nilai Anda D";
+	} else {
+		echo "Nilai Anda E";
+	}
+?>
