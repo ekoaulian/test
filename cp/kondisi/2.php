dc41393840e225396154c59a395f5bd0a6bee5b6
ekoaulian <ekoas1997@gmail.com>
2019-11-26 23:35:10 +0700

    cp1

--- cp/kondisi/2.php
+++ cp/kondisi/2.php
@@ -1,8 +1,11 @@
 <?php 
-$nilai = 50;
+$nilai = 25;
 
-if ($nilai >= 1) {
-	echo "Anda lulus, nilai anda " . $nilai;
+if ($nilai >= 50) {
+	echo "Anda lulus, nilai anda = " . $nilai;
+}
+if ($nilai < 50) {
+	echo "Maaf anda belum lulus, nilai anda = " . $nilai;
 }
 
 ?>
